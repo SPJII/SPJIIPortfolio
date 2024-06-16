c7551bea7c16473d7159db24575e99ba3c34207a
Steven P Jones <106358275+SPJII@users.noreply.github.com>
2024-06-16 12:35:01 -0400

    Update index.js to have new resume
    changed the path names on lines 86 & 87

--- index.js
+++ index.js
@@ -83,8 +83,8 @@ document.addEventListener("DOMContentLoaded", function() {
     resumeButton.addEventListener("click", function() {
         // Create an anchor element
         var downloadLink = document.createElement("a");
-        downloadLink.href = "UC_Resume_SPJII.pdf"; // Replace with the path to your PDF file
-        downloadLink.download = "UC_Resume_SPJII.pdf"; // Specify the file name for download
+        downloadLink.href = "UC Resume SPJII.pdf"; // Replace with the path to your PDF file
+        downloadLink.download = "UC Resume SPJII.pdf"; // Specify the file name for download
         downloadLink.target = "_blank"; // Open the PDF file in a new tab
         document.body.appendChild(downloadLink);
         downloadLink.click(); // Simulate a click on the download link
